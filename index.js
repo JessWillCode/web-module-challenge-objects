26c607d3933dec319aec9efdbc79f655742a1f33
Jessica Kitchen <jess@Jessicas-Mini.lan1>
2021-11-04 17:11:44 -0400

    finished regular project tasks

--- index.js
+++ index.js
@@ -107,16 +107,17 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(name, rating, feedback){
-  const array = [];
+function addReview(array, name, rating, feedback){
   const newReview = {
   name, 
   rating,
-  review: feedback
+  feedback
 }
 array.push(newReview);
 return array;
 }
+
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
@@ -129,7 +130,7 @@ Use the getReviewByIndex function below to do the following:
 */
 
 function getReviewByIndex(array, number) {
-  return `${this.name} gave the restaurant a ${this.rating} star review, and their feedback was: ${this.feedback}`;
+  return `${array[number].name} gave the restaurant a ${array[number].rating} star review, and their feedback was: ${array[number].feedback}`;
 }
 
 console.log(getReviewByIndex(reviews, 5));
@@ -147,10 +148,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`;
 } 
 
+console.log(reviews);
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
